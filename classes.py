43491010d6f2b74601461aa7eb092407de07cb19
Walkerovski <01158617@pw.edu.pl>
2022-06-12 16:35:38 +0200

    Collision implementation

--- classes.py
+++ classes.py
@@ -1,9 +1,10 @@
-from random import randint
+from random import randint, gauss
 from time import sleep
 
-# stale fizyczne ?
-ACCELERATION_X = 0
-ACCELERATION_Y = 0
+
+ACCELERATION_X = 0.1
+ACCELERATION_Y = 0.1
+MAX_X_VELOCITY = 1
 
 
 class Atom:
@@ -15,33 +16,25 @@ class Atom:
         self.max_deviation = max_deviation
         self.size = size
         self.temperature = temperature
-        self.velocity_x = 0
-        self.velocity_y = 0
 
-    def random_move(self, temperature):
+    def random_move(self):
         way = randint(1, 4) % 4
         # try move right
         if way == 0:
-            if self.actual_location_x + 0.5 <= self.start_location_x + self.max_deviation:
-                self.actual_location_x += 0.5
+            if self.actual_location_x + self.temperature <= self.start_location_x + self.max_deviation:
+                self.actual_location_x += self.temperature
         # try to move left
         elif way == 1:
-            if self.actual_location_x - 0.5 >= self.start_location_x - self.max_deviation:
-                self.actual_location_x -= 0.5
+            if self.actual_location_x - self.temperature >= self.start_location_x - self.max_deviation:
+                self.actual_location_x -= self.temperature
         # try to move up
         elif way == 2:
-            if self.actual_location_y + 0.5 <= self.start_location_y + self.max_deviation:
-                self.actual_location_y += 0.5
+            if self.actual_location_y + self.temperature <= self.start_location_y + self.max_deviation:
+                self.actual_location_y += self.temperature
         # try to move down
         else:
-            if self.actual_location_y - 0.5 >= self.start_location_y - self.max_deviation:
-                self.actual_location_y -= 0.5
-
-    def set_dim_x(self, new_dim_x):
-        self.actual_location_x = new_dim_x
-
-    def set_dim_y(self, new_dim_y):
-        self.actual_location_y = new_dim_y
+            if self.actual_location_y - self.temperature >= self.start_location_y - self.max_deviation:
+                self.actual_location_y -= self.temperature
 
     def set_max_deviation(self, new_max_deviation):
         self.max_deviation = new_max_deviation
@@ -51,12 +44,6 @@ class Atom:
 
     def set_temperature(self, new_temperature):
         self.temperature = new_temperature
-
-    def set_new_velocity_x(self, new_velocity_x):
-        self.velocity_x = new_velocity_x
-
-    def set_new_velocity_y(self, new_velocity_y):
-        self.velocity_y = new_velocity_y
 
     def get_dim_x(self):
         return self.actual_location_x
@@ -73,91 +60,94 @@ class Atom:
     def get_temperature(self):
         return self.temperature
 
-    def get_new_velocity_x(self):
-        return self.velocity_x
-
-    def get_new_velocity_y(self):
-        return self.velocity_y
-
 
 class Electron():
     def __init__(self, start_location_x, start_location_y, size):
         self.actual_location_x = start_location_x
         self.actual_location_y = start_location_y
         self.size = size
-        self.acceleration_x = ACCELERATION_X  # do ustalenia ze wzorow
-        self.acceleration_y = ACCELERATION_Y  # do ustalenia ze wzorow
+        self.acceleration_x = ACCELERATION_X
+        self.acceleration_y = ACCELERATION_Y
         self.velocity_x = 0
         self.velocity_y = 0
-        self.velocity_without_atoms = 0#VELOCITY_WITHOUT_ATOMS  # x-owa predkosc
-        #  ^ nie jest to chyba predkosc maksymalna jaka moze miec elektron
+        self.max_x_velocity = MAX_X_VELOCITY
 
-    def move(self):
-        self.change_velocity_after_collision_with_atom(0,0)
-        self.set_new_location_x(self.actual_location_x + 3)
+    def move(self, atoms):
+        self.set_new_velocity_x()
+        self.set_new_velocity_y()
+        self.set_new_location_x()
+        self.set_new_location_y()
+        self.check_collision(atoms)
 
-    def change_velocity_after_collision_with_atom(self, atom_velocity_x, atom_velocity_y):
-        # prawo zachowania pedu ?
-        pass
+    def check_collision(self, atoms):
+        for a in atoms:
+            if self.actual_location_x > a.get_dim_x() - 1.7 \
+                and self.actual_location_x < a.get_dim_x() + 1.7:
+                if self.actual_location_y > a.get_dim_y() - 1.7 \
+                    and self.actual_location_y < a.get_dim_y() + 1.7:
+                    self.change_velocity_after_collision_with_atom(a)
 
-    def set_new_location_x(self, new_location_x):
-        self.actual_location_x = new_location_x
+    def change_velocity_after_collision_with_atom(self, atom):
+        self.velocity_x = -self.velocity_x * 0.7
+        if self.actual_location_y > atom.get_dim_y() - 1.7 \
+            and self.actual_location_y < atom.get_dim_y():
+                self.velocity_y = -1
+        else:
+            self.velocity_y = 1
 
-    def set_new_location_y(self, new_location_y):
-        self.actual_location_y = new_location_y
+    def set_new_location_x(self):
+        self.actual_location_x += self.velocity_x
+        if self.actual_location_x > 90:
+            self.actual_location_x = 0
+            self.actual_location_y += gauss(0, 5)
 
-    def set_new_size(self, new_size):
-        self.size = new_size
+    def set_new_location_y(self):
+        self.actual_location_y += self.velocity_y
+        if self.actual_location_y > 50:
+            self.actual_location_y = 1
+        if self.actual_location_y < 0:
+            self.actual_location_y = 49
 
-    def set_new_acceleration_x(self, new_acceleration_x):
-        self.acceleration_x = new_acceleration_x
+    def set_new_velocity_x(self):
+        self.velocity_x += self.acceleration_x
+        self.velocity_x = min(self.velocity_x, self.max_x_velocity)
 
-    def set_new_acceleration_y(self, new_acceleration_y):
-        self.acceleration_y = new_acceleration_y
+    def set_new_velocity_y(self):
+        if abs(self.velocity_y) < self.acceleration_y:
+            self.velocity_y = 0 
+        elif self.velocity_y > 0:
+            self.velocity_y -= self.acceleration_y
+        else:
+            self.velocity_y += self.acceleration_y
 
-    def set_new_velocity_x(self, new_velocity_x):
-        self.velocity_x = new_velocity_x
-
-    def set_new_velocity_y(self, new_velocity_y):
-        self.velocity_y = new_velocity_y
-
-    def set_new_velocity_without_atoms(self, new_velocity_without_atoms):
-        self.velocity_without_atoms = new_velocity_without_atoms
-
-    def get_new_location_x(self):
+    def get_location_x(self):
         return self.actual_location_x
 
-    def get_new_location_y(self):
+    def get_location_y(self):
         return self.actual_location_y
-
-    def get_new_size(self):
+    
+    def get_size(self):
         return self.size
 
-    def get_new_acceleration_x(self):
-        return self.acceleration_x
-
-    def get_new_acceleration_y(self):
-        return self.acceleration_y
-
-    def get_new_velocity_x(self):
+    def get_velocity_x(self):
         return self.velocity_x
 
-    def get_new_velocity_y(self):
+    def get_velocity_y(self):
         return self.velocity_y
+    
+    def get_max_x_velocity(self):
+        return self.max_x_velocity
 
-    def get_new_velocity_without_atoms(self):
-        return self.velocity_without_atoms
 
 
 if __name__ == "__main__":
-    atom1 = Atom(1, 1, 10, 10, 5)
+    atom1 = Atom(1, 1, 2, 50, 0.1)
     print(atom1.get_temperature())
     print(atom1.get_size())
     print(atom1.get_max_deviation())
     print(atom1.get_dim_y())
     print(atom1.get_dim_x())
     while True:
-        # ruchy atomu zaleza jakos od temperatury, trzeba by to jakos wykorzystac
-        atom1.random_move(atom1.get_temperature())
+        atom1.random_move()
         print(atom1.get_dim_x(), atom1.get_dim_y())
         sleep(0.1)
--- plot.py
+++ plot.py
@@ -1,44 +1,58 @@
-import matplotlib.pyplot as plt
+from random import gauss
+from matplotlib import pyplot as plt
 import numpy as np
 from classes import Atom, Electron
 
-x = [x for x in range(10, 101, 10)]
-y = [y for y in range(10, 51, 10)]
+TEMPERATURE = 0.5
+NUMBER_OF_ELECTRONS = 50
+SIZE = 1000
+MAX_DEVIATION = 2
+
+x = [x for x in range(5, 86, 10)]
+y = [y for y in range(5, 46, 10)]
 
 atoms = []
 for a in x:
     for b in y:
-        atom = Atom(a, b, 2, 50, 5)
+        atom = Atom(a, b, MAX_DEVIATION, SIZE, TEMPERATURE)
         atoms.append(atom)
 
 electrons = []
-for b in y:
-    electron = Electron(0, b, 2)
+for _ in range(0, NUMBER_OF_ELECTRONS):
+    electron = Electron(gauss(-10, 10), gauss(25, 15), 10)
     electrons.append(electron)
 
 plt.ion()
-fig, ax = plt.subplots()
+fig = plt.figure(figsize=(18, 9), dpi=100)
+ax = fig.add_subplot()
+fig.canvas.draw()
+axbackground = fig.canvas.copy_from_bbox(ax.bbox)
+plt.show(block=False)
 
 while(1):
+    drif = 0
     for atom in atoms:
         x = atom.get_dim_x()
         y = atom.get_dim_y()
-        sizes = atom.get_size()
         colors = "red"
-        ax.scatter(x, y, s=sizes, c=colors)
-        atom.random_move(10)
+        ax.scatter(x, y, s=SIZE, c=colors)
+        atom.random_move()
     
     for electron in electrons:
-        x = electron.get_new_location_x()
-        y = electron.get_new_location_y()
-        sizes = electron.get_new_size()
+        x = electron.get_location_x()
+        y = electron.get_location_y()
+        sizes = electron.get_size()
         colors = "blue"
         ax.scatter(x, y, s=sizes, c=colors)
-        electron.move()
+        electron.move(atoms)
+        drif += electron.get_velocity_x()
 
-    ax.set(xlim=(0, 110), xticks=np.arange(10, 101, 10),
-        ylim=(0, 60), yticks=np.arange(10, 51, 10))
+    ax.set(xlim=(0, 90), xticks=np.arange(10, 81, 10),
+        ylim=(0, 50), yticks=np.arange(10, 41, 10))
     
-    fig.canvas.draw()
+    fig.canvas.restore_region(axbackground)
+    fig.canvas.blit(ax.bbox)
     fig.canvas.flush_events()
     ax.clear()
+    drif /= len(electrons)
+    print(drif / electrons[0].get_max_x_velocity())
